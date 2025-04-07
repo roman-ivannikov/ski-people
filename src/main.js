2592b13b5b8a843b7c9a7aa25e5002f64b1f9995
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2025-04-07 16:50:55 +0700

    add swiper slider module

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import './normalize.css';
 import './style.scss';
+
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = new Swiper(".product__slider", {
+    spaceBetween: 10,
+    modules: [ Navigation ],
+    navigation: {
+        nextEl: ".slider__arrow-right",
+        prevEl: ".slider__arrow-left",
+    }
+});
+
