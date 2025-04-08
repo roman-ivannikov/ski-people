173a5db6357d4c2fb7ed7e0e2ae59f20bd2e6d1d
roman-ivannikov <ivannikov.roman@mail.ru>
2025-04-08 20:00:53 +0700

    work on swiper slider

--- src/main.js
+++ src/main.js
@@ -2,16 +2,37 @@ import './normalize.css';
 import './style.scss';
 
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Thumbs } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-const swiper = new Swiper(".product__slider", {
-    spaceBetween: 10,
-    modules: [ Navigation ],
-    navigation: {
-        nextEl: ".slider__arrow-right",
-        prevEl: ".slider__arrow-left",
+const thumb = new Swiper(".product__slider-thumbnails", {
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+    enabled: false,
+    breakpoints: {
+        768: {
+            spaceBetween: 14
+        },
+        1024: {
+            spaceBetween: 12
+        },
+        1440: {
+            spaceBetween: 16
+        }
     }
 });
 
+const swiper = new Swiper(".product__slider", {
+    spaceBetween: 0,
+    loop: true,
+    modules: [ Navigation, Thumbs ],
+    navigation: {
+        nextEl: ".slider__arrow-right",
+        prevEl: ".slider__arrow-left",
+    },
+    thumbs: {
+        swiper: thumb
+    }
+});
